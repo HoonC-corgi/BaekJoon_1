1e25d0d2c4c54e30b6e30adb0e5e5fa1351d48f0
성훈쨩 <gimseonghun@seonghunjjyang-ui-MacBookAir.local>
2023-01-01 22:11:40 +0900

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int arr[][] = new int[9][9];
+        int max=0;
+        int row=0;
+        int column=0;
+
+        for(int i=0; i<9; i++) {
+            for(int j=0; j<9; j++) {
+                arr[i][j] = scanner.nextInt();
+                if(max<arr[i][j]) {
+                    max=arr[i][j];
+                    row=i+1;
+                    column=j+1;
+                }
+            }
+        }
+
+        System.out.println(max);
+        System.out.print(row + " " + column);
+        scanner.close();
+    }
+}
